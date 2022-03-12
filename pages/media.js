d018363050ca2d581ba1761fb17f6f80d68eb736
Alex Zenin <alex@alexzenin.com>
2022-03-12 21:28:37 +1100

    feat: add media page

---
+++ pages/media.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createClient } from '../prismicio'
+import Layout from "../components/Layout/Layout"
+import SliceZone from '../components/SliceZone'
+
+export default function Media({ page }) {
+    return (
+        <Layout>
+            <SliceZone slices={page.data.body} />
+        </Layout>
+    )
+}
+
+export async function getStaticProps({ previewData }) {
+    const client = createClient({ previewData })
+
+    const page = await client.getSingle('media')
+
+    return {
+        props: { page },
+    }
+}
